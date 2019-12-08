c0cad8020507c3330b38053d25007cf700e7e404
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 12:34:31 -0500

    import BookList

--- graphql-frontend/src/App.js
+++ graphql-frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from './Components/BookList';
 
 function App() {
   return (
     <div className="Main">
       <h1>Adrian's Reading List</h1>
+      <BookList/>
     </div>
   );
 }
