a8e4e21eebdb6c5fcb800aade499850980c34127
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 17:57:32 -0500

    connect apollo client to react

--- graphql-frontend/src/index.js
+++ graphql-frontend/src/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
+import { render } from 'react-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, gql } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-const client = new ApolloClient({
+const cache = new InMemoryCache();
+const link = new HttpLink({
   uri: 'http://localhost:4000/graphql'
 });
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+client
+  .query({
+    query: gql`
+    {
+      books {
+        name
+        genre
+        author {
+          name
+          age
+        }
+      }
+    }
+  `
+  })
+  .then(result => console.log(result));
+  
+  const Apollo = () => (
+    <ApolloProvider client={client}>
+      <div>
+        <App />
+      </div>
+    </ApolloProvider>
+  );
+  
+  render(<Apollo />, document.getElementById('root'));
+
+  // ReactDOM.render(<App />, document.getElementById('root'));
