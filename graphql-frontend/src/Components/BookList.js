56ab4b0668be4f99fd1ee400bf34d0b538e83474
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 12:32:24 -0500

    stub BookList

---
+++ graphql-frontend/src/Components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <ul idd="book-list">
+        <li>Book Name</li>
+        <li>Book Name</li>
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
