2b3a4a45e56e8a88070aee86a1a6c042180ae8f3
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 18:58:25 -0500

    render list of books

--- graphql-frontend/src/Components/BookList.js
+++ graphql-frontend/src/Components/BookList.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-function BookList() {
-  return (
-    <div>
-      <ul idd="book-list">
-        <li>Book Name</li>
-        <li>Book Name</li>
-        <li>Book Name</li>
-      </ul>
-    </div>
-  );
+const GET_BOOKS = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+function BookList ({ onBooksSelected }) {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  // return null;
+  return data.books.map(({ name, genre, id }) => (
+    <li key={id}>{name}</li>
+  ));
 }
+
+// function BookList() {
+//   return (
+//     <div>
+//       <ul idd="book-list">
+//         <li>Book Name</li>
+//         <li>Book Name</li>
+//         <li>Book Name</li>
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default BookList;
