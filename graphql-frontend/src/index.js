8c3fe1c9d07457dc987ade792162ecb829f61fed
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 13:07:23 -0500

    create apollo client

--- graphql-frontend/src/index.js
+++ graphql-frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
