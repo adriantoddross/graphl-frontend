00387ef9b65f981fefa77fb21783c761fa2724b2
Adrian Ross <adriantoddross@gmail.com>
2019-12-08 20:13:45 -0500

    render authors as options

--- graphql-frontend/src/Components/AddBook.js
+++ graphql-frontend/src/Components/AddBook.js
@@ -2,49 +2,54 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 
-const GET_AUTHORS = gql`
-  {
-    authors {
-      name
-      id
-    }
+
+const GET_AUTHORS =gql`
+{
+  authors {
+    name
+    id
   }
+}
 `;
 
-function AddBook ({ onBooksSelected }) {
+function AddAuthors () {
   const { loading, error, data } = useQuery(GET_AUTHORS);
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
-  // return null;
-  // return data.author.map(({ name, id }) => (
-  //   <li key={id}>{name}</li>
-  // ));
-  return (
-    <form id="add-book">
-
-        <div className="field">
-          <label>Book name:</label>
-          <input type="text"/>
-        </div>
-        
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text"/>
-        </div>
-
-        <div className="field">
-          <label>Author:</label>
-          <select>
-            <option>Select Author</option>
-          </select>
-        </div>
-
-        <button></button>
-
-      </form>
-  );
+  return data.authors.map(({ name, id }) => (
+    <option key={id}>{name}</option>
+  ));
 }
+
+function AddBook() {
+
+    return (
+      <form id="add-book">
+
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text"/>
+      </div>
+      
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text"/>
+      </div>
+
+      <div className="field">
+        <label>Author:</label>
+        <select>
+          <option>Select Author</option>
+          {AddAuthors()}
+        </select>
+      </div>
+
+      <button></button>
+
+    </form>
+    );
+  }
 
 export default AddBook;
