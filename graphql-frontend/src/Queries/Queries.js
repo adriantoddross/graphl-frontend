ba515784d63d9b1faa49bcafa265e27bfb4ff0bc
Adrian Ross <adriantoddross@gmail.com>
2019-12-09 06:26:41 -0500

    add and export queries

---
+++ graphql-frontend/src/Queries/Queries.js
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-boost';
+
+
+const GET_BOOKS = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const GET_AUTHORS =gql`
+{
+  authors {
+    name
+    id
+  }
+}
+`;
+
+export {GET_BOOKS, GET_AUTHORS};
